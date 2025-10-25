4c222b93d7019c4d953e7d4386f236b37a648764
Reagan255 <reaganwafula46@gmail.com>
2025-10-25 12:03:25 +0100

    Create insert books.js

---
+++ insert books.js
@@ -0,0 +1,102 @@
+db.books.insertMany([
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Classic",
+    published_year: 1925,
+    price: 12.99,
+    in_stock: true,
+    pages: 180,
+    publisher: "Scribner"
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Classic",
+    published_year: 1960,
+    price: 14.99,
+    in_stock: true,
+    pages: 281,
+    publisher: "J.B. Lippincott & Co."
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian",
+    published_year: 1949,
+    price: 11.99,
+    in_stock: false,
+    pages: 328,
+    publisher: "Secker & Warburg"
+  },
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    published_year: 1937,
+    price: 19.99,
+    in_stock: true,
+    pages: 310,
+    publisher: "Allen & Unwin"
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    genre: "Classic",
+    published_year: 1951,
+    price: 10.49,
+    in_stock: true,
+    pages: 234,
+    publisher: "Little, Brown and Company"
+  },
+  {
+    title: "The Hunger Games",
+    author: "Suzanne Collins",
+    genre: "Science Fiction",
+    published_year: 2008,
+    price: 15.99,
+    in_stock: false,
+    pages: 374,
+    publisher: "Scholastic Press"
+  },
+  {
+    title: "Dune",
+    author: "Frank Herbert",
+    genre: "Science Fiction",
+    published_year: 1965,
+    price: 22.99,
+    in_stock: true,
+    pages: 412,
+    publisher: "Chilton Books"
+  },
+  {
+    title: "The Fault in Our Stars",
+    author: "John Green",
+    genre: "Romance",
+    published_year: 2012,
+    price: 13.99,
+    in_stock: true,
+    pages: 313,
+    publisher: "Dutton Books"
+  },
+  {
+    title: "Harry Potter and the Sorcerer’s Stone",
+    author: "J.K. Rowling",
+    genre: "Fantasy",
+    published_year: 1997,
+    price: 29.99,
+    in_stock: false,
+    pages: 309,
+    publisher: "Bloomsbury"
+  },
+  {
+    title: "The Girl with the Dragon Tattoo",
+    author: "Stieg Larsson",
+    genre: "Thriller",
+    published_year: 2005,
+    price: 17.99,
+    in_stock: true,
+    pages: 465,
+    publisher: "Norstedts Förlag"
+  }
+]);
